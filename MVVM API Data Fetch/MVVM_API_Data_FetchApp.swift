51474bc1bfc2e1d42698d019c01d264716f37064
Masrafi <masrafianam@gmail.com>
2024-07-28 14:21:19 +0600

    Initial Commit

---
+++ MVVM API Data Fetch/MVVM_API_Data_FetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_API_Data_FetchApp.swift
+//  MVVM API Data Fetch
+//
+//  Created by Md Khorshed Alam on 28/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_API_Data_FetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
